f426aa7359fe8ea6ebc6f67809181149e21a7ae1
Salim Abubakar <salimabubakar@Salims-MacBook-Pro.local>
2021-09-26 18:21:30 +0100

    Icon component

---
+++ src/components/Icon.vue
@@ -0,0 +1,15 @@
+<template> </template>
+<script>
+export default {
+  name: "Icon",
+  data() {
+    return {};
+  },
+};
+</script>
+
+<style scoped>
+.selected {
+  background-color: #fff;
+}
+</style>
